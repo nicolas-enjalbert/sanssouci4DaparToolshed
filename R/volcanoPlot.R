116bb0439016cfd20c8dd69615183f37cecd4992
PomKokca <manong2085@orange.fr>
2026-05-22 16:47:59 +0200

    Modifications on the volcano plot

--- R/volcanoPlot.R
+++ R/volcanoPlot.R
@@ -9,6 +9,8 @@
 #' This vector must have the same length as `pval`.
 #' @param th_pval A `numeric(1)` which is the p-value threshold under which 
 #' proteins are selected.
+#' @param th_qval A `numeric(1)` which is the q-value threshold under which 
+#' proteins are selected.
 #' @param th_logfc A `numeric(1)` which is the absolute log(fold change) 
 #' threshold above which proteins are selected.
 #' @param conditions A `character(2)` containing the names of both conditions.
@@ -30,7 +32,8 @@
 
 VolcanoPlot_ggplot <- function(pval,
                                logFC,
-                               th_pval = 0,
+                               th_pval = 1,
+                               th_qval = 1,
                                th_logfc = 0,
                                conditions = NULL,
                                pal = NULL) {
@@ -55,8 +58,17 @@ VolcanoPlot_ggplot <- function(pval,
   if (length(th_pval) != 1){
     stop("'th_pval' must be of length 1.")
   }
-  if (th_pval < 0){
-    stop("'th_pval' must be positive.")
+  if ((th_pval < 0) | (th_pval > 1)){
+    stop("'th_pval' must be between 0 and 1.")
+  }
+  if (!is.numeric(th_qval)){
+    stop("'th_qval' must be numeric.")
+  }
+  if (length(th_qval) != 1){
+    stop("'th_qval' must be of length 1.")
+  }
+  if ((th_qval < 0) | (th_qval > 1)){
+    stop("'th_qval' must be between 0 and 1.")
   }
   if (!is.numeric(th_logfc)){
     stop("'th_logfc' must be numeric.")
@@ -66,6 +78,10 @@ VolcanoPlot_ggplot <- function(pval,
   }
   if (th_logfc < 0){
     stop("'th_logfc' must be positive.")
+  }
+  
+  if ((th_pval < 1) && (th_qval < 1)) {
+    warning("Filtering both on p-values and BH-adjusted p-values")
   }
   
   if (is.null(conditions)){
@@ -80,22 +96,64 @@ VolcanoPlot_ggplot <- function(pval,
   if (is.null(pal)) {
     pal <- list(In = "orange", Out = "gray")
   } else if (length(pal) != 2) {
-    warning("The palette must be a list of two items: In and Out. Set to default.")
+    warning("'pal' must be of length 2. Colors set to default.")
     pal <- list(In = "orange", Out = "gray")
   } else {
     pal <- list(In = pal[1], Out = pal[2])
   }
   
-  df <- data.frame(pval = pval, logFC = logFC)
+  logpval <- -log10(pval)
+  
+  adjp <- p.adjust(pval, method = "BH")  ## adjusted p-values
+  pval_sel <- which((adjp <= th_qval) &           ## selected by q-value
+                   (pval <= th_pval))        ##          or p-value
+  pval_thr <- Inf
+  if (length(pval_sel) > 0) {
+    pval_thr <- min(logpval[pval_sel])       ## threshold on the log(p-value) scale
+  }
+  
+  ## gene selections
+  sel1 <- which(logpval >= pval_thr & logFC >= th_logfc)
+  sel2 <- which(logpval >= pval_thr & logFC <= -th_logfc)
+  sel12 <- sort(union(sel1, sel2))
+  
+  ## post hoc bounds in selections
+  thr <- length(pval)
+  
+  n1 <- length(sel1)
+  FP1 <- maxFP(pval[sel1], thr = thr)
+  TP1 <- n1 - FP1
+  FDP1 <- round(FP1/max(n1, 1), 2)
+  
+  n2 <- length(sel2)
+  FP2 <- maxFP(pval[sel2], thr = thr)
+  TP2 <- n2 - FP2
+  FDP2 <- round(FP2/max(n2, 1), 2)
+  
+  n12 <- length(sel12)
+  FP12 <- maxFP(pval[sel12], thr = thr)
+  TP12 <- n12 - FP12
+  FDP12 <- round(FP12/max(n12, 1), 2)
+  
+  
+  df <- data.frame(pval = logpval, logFC = logFC)
   
   # Significant / non-significant groups
-  df$isDiff <- ifelse(df$pval >= th_pval & abs(df$logFC) >= th_logfc,
+  df$isDiff <- ifelse(df$pval >= pval_thr & abs(df$logFC) >= th_logfc,
                       "In", 
                       "Out")
   
+  if (title != ""){
+    title <- paste0(title, " - ", n12, " proteins selected \n",
+                    "At least ", TP12, " true positives (FDP <= ", FDP12, ")")
+  } else {
+    title <- paste0(n12, " proteins selected \n",
+                    "At least ", TP12, " true positives (FDP <= ", FDP12, ")")
+  }
+  
   p <- ggplot2::ggplot(df,
                        ggplot2::aes(x = logFC, y = pval, color = isDiff)) +
-    ggplot2::geom_hline(yintercept = th_pval,
+    ggplot2::geom_hline(yintercept = pval_thr,
                         linetype = "dashed",
                         color = "grey") +
     ggplot2::geom_vline(xintercept = c(-th_logfc, th_logfc),
