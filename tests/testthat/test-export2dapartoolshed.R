8acdaa4c86dfd9ec6a643f3e7a791b9482f0dccb
nicolas-enjalbert <nicolasec@yahoo.fr>
2026-05-26 20:22:23 +0200

    add export2dapartoolshed.R function

---
+++ tests/testthat/test-export2dapartoolshed.R
@@ -0,0 +1,189 @@
+numdata <- data.frame(S1 = c(2, 5, 9, 10, 7, 8, 6, 8, 6, 7),
+                      S2 = c(7, 8, 5, 5, 7, 9, 9, 4, 9, 1),
+                      S3 = c(4, 1, 4, 9, 6, 8, 5, 3, 4, 3),
+                      S4 = c(9, 8, 9, 8, 3, 6, 4, 1, 4, 5),
+                      names = paste0("Prot", seq_len(10)))
+nb_prot <- nrow(numdata)
+nb_samples <- ncol(numdata) - 1
+qfeature_obj <- QFeatures::readQFeatures(numdata, quantCols = seq_len(4), fnames = "names", name = "datatest")
+
+coldata <- data.frame(quantCols = c("S1", "S2", "S3", "S4"),
+                      Condition = c("C1", "C1", "C2", "C2"),
+                      Bio.Rep   = as.character(seq_len(4)))
+rownames(coldata) <- coldata$quantCols
+SummarizedExperiment::colData(qfeature_obj) <- coldata
+
+rownames(numdata) <- numdata$names
+numdata <- numdata[,colnames(numdata) != "names"]
+sanssouci_init <- SansSouci(Y = as.matrix(numdata), groups = (coldata$Condition == "C1")*1)
+
+B = 10
+sanssouci_obj <- fit(sanssouci_init, B = B, alpha = 0.5)
+
+
+test_that("Unit/fctal test of export2dapartoolshed : good", {
+  ## dont give qfeature_obj ; expect data.frame object
+  pval_thr <- 0.5
+  logFC_thr <- 0.001
+  res <- export2dapartoolshed(sanssouci_obj, pval_threshold = pval_thr,
+                              Foldchange_thlogFC = logFC_thr)
+
+  expect_true(is.data.frame(res))
+  expect_equal(colnames(res), c("name_prot", "pval", "logFC", "sel_1"))
+  expect_equal(names(attributes(res)), c("names", "row.names", "nb_permutation",
+                                         "rowTestFun", "class"))
+  # expect_equal(names(attributes(res$sel_1)), c("thr_pval", "thr_logFC",
+  #                                              "n_sel",
+  #                                       "FDP", "TP")) # tested with consistenc
+  expect_equal(dim(res), c(nb_prot, 4))
+  expect_equal(res$name_prot, rownames(numdata))
+
+  ##Consistency with sanssouci
+  pval <- pValues(sanssouci_obj)
+  logFC <- foldChanges(sanssouci_obj)
+  S <- (pval < pval_thr) & (abs(logFC) > logFC_thr)
+  PHB <- predict(sanssouci_obj, S = which(S), what = c("FDP", "TP"))
+
+  expect_equal(as.vector(res$pval), as.vector(pval))
+  expect_equal(as.vector(res$logFC), as.vector(logFC))
+  expect_equal(as.vector(res$sel_1), as.vector(S*1))
+
+  expect_equal(attr(res, "nb_permutation"), B)
+  expect_equal(attr(res, "rowTestFun"), "rowWelchTests")
+
+  expect_equal(attributes(res$sel_1),
+               list("thr_pval" = pval_thr,
+                    "thr_logFC" = logFC_thr,
+                    "n_sel" = sum(S),
+                    "FDP" = PHB[["FDP"]],
+                    "TP" = PHB[["TP"]]))
+
+  #### test with qfeature_obj : expect QFeature object
+
+
+  res1 <- export2dapartoolshed(qfeature_obj = qfeature_obj,
+                               sanssouci_obj = sanssouci_obj,
+                               pval_threshold = pval_thr,
+                               Foldchange_thlogFC = logFC_thr)
+  expect_equal(class(res1), class(qfeature_obj))
+  res <- metadata(res1)$posthoc_selection
+  expect_true(is.data.frame(res))
+  expect_equal(colnames(res), c("name_prot", "pval", "logFC", "sel_1"))
+  expect_equal(names(attributes(res)), c("names", "row.names", "nb_permutation",
+                                         "rowTestFun", "class"))
+  # expect_equal(names(attributes(res$sel_1)), c("thr_pval", "thr_logFC",
+  #                                              "n_sel",
+  #                                       "FDP", "TP")) # tested with consistenc
+  expect_equal(dim(res), c(nb_prot, 4))
+  expect_equal(res$name_prot, rownames(numdata))
+
+  ##Consistency with sanssouci
+  pval <- pValues(sanssouci_obj)
+  logFC <- foldChanges(sanssouci_obj)
+  S <- (pval < pval_thr) & (abs(logFC) > logFC_thr)
+  PHB <- predict(sanssouci_obj, S = which(S), what = c("FDP", "TP"))
+
+  expect_equal(as.vector(res$pval), as.vector(pval))
+  expect_equal(as.vector(res$logFC), as.vector(logFC))
+  expect_equal(as.vector(res$sel_1), as.vector(S*1))
+
+  expect_equal(attr(res, "nb_permutation"), B)
+  expect_equal(attr(res, "rowTestFun"), "rowWelchTests")
+
+  expect_equal(attributes(res$sel_1),
+               list("thr_pval" = pval_thr,
+                    "thr_logFC" = logFC_thr,
+                    "n_sel" = sum(S),
+                    "FDP" = PHB[["FDP"]],
+                    "TP" = PHB[["TP"]]))
+
+  pval_thr2 <- 0.05
+  logFC_thr2 <- 0.04
+  res2 <- export2dapartoolshed(qfeature_obj = res1 ,
+                               sanssouci_obj = sanssouci_obj,
+                               pval_threshold = pval_thr2,
+                               Foldchange_thlogFC = logFC_thr2)
+  expect_equal(class(res2), class(qfeature_obj))
+  res <- metadata(res2)$posthoc_selection
+  expect_true(is.data.frame(res))
+  expect_equal(colnames(res), c("name_prot", "pval", "logFC", "sel_1", "sel_2"))
+  expect_equal(names(attributes(res)), c("names", "row.names", "nb_permutation",
+                                         "rowTestFun", "class"))
+  # expect_equal(names(attributes(res$sel_1)), c("thr_pval", "thr_logFC",
+  #                                              "n_sel",
+  #                                       "FDP", "TP")) # tested with consistenc
+  expect_equal(dim(res), c(nb_prot, 5))
+  expect_equal(res$name_prot, rownames(numdata))
+
+  ##Consistency with sanssouci : sel_1 is not modified
+  pval <- pValues(sanssouci_obj)
+  logFC <- foldChanges(sanssouci_obj)
+  S <- (pval < pval_thr) & (abs(logFC) > logFC_thr)
+  PHB <- predict(sanssouci_obj, S = which(S), what = c("FDP", "TP"))
+
+  expect_equal(as.vector(res$pval), as.vector(pval))
+  expect_equal(as.vector(res$logFC), as.vector(logFC))
+  expect_equal(as.vector(res$sel_1), as.vector(S*1))
+
+  expect_equal(attr(res, "nb_permutation"), B)
+  expect_equal(attr(res, "rowTestFun"), "rowWelchTests")
+
+  expect_equal(attributes(res$sel_1),
+               list("thr_pval" = pval_thr,
+                    "thr_logFC" = logFC_thr,
+                    "n_sel" = sum(S),
+                    "FDP" = PHB[["FDP"]],
+                    "TP" = PHB[["TP"]]))
+
+  ## Consistency with sanssouci : test sel_2
+  S <- (pval < pval_thr2) & (abs(logFC) > logFC_thr2)
+  PHB <- predict(sanssouci_obj, S = which(S), what = c("FDP", "TP"))
+
+  expect_equal(as.vector(res$sel_2), as.vector(S*1))
+
+  expect_equal(attributes(res$sel_2),
+               list("thr_pval" = pval_thr2,
+                    "thr_logFC" = logFC_thr2,
+                    "n_sel" = sum(S),
+                    "FDP" = PHB[["FDP"]],
+                    "TP" = PHB[["TP"]]))
+
+
+
+})
+
+test_that("Unit test of export2dapartoolshed : errors", {
+  # Do not give a sanssouci object
+  expect_error(export2dapartoolshed(sanssouci_obj = 10,
+                       pval_threshold = 1,
+                       Foldchange_thlogFC = 1),
+               regexp = "sanssouci_obj must be a 'SansSouci' class object.")
+
+  # Do not give a qfeatures object
+  expect_error(export2dapartoolshed(sanssouci_obj = sanssouci_obj,
+                                    qfeature_obj = 10,
+                                    pval_threshold = 1,
+                                    Foldchange_thlogFC = 1),
+               regexp = "qfeature_obj must be a 'QFeatures' class object.")
+
+
+  res1 <- export2dapartoolshed(qfeature_obj = qfeature_obj,
+                               sanssouci_obj = sanssouci_obj,
+                               pval_threshold = 0.5,
+                               Foldchange_thlogFC = 0.5)
+  #try to use another calibration parameters : B
+  sanssouci_obj_F <- fit(sanssouci_init, alpha = 0.5, B = 1)
+  expect_error(export2dapartoolshed(sanssouci_obj = sanssouci_obj_F,
+                                    pval_threshold = 1,
+                                    qfeature_obj = res1,
+                                    Foldchange_thlogFC = 1),
+               regexp = "Number of permutation used must be  10 as previously saved.")
+  #try to use another calibration parameters : rowTestFun
+  sanssouci_obj_F <- fit(sanssouci_init, alpha = 0.5,
+                         rowTestFUN = rowWilcoxonTests, B = 10)
+  expect_error(export2dapartoolshed(sanssouci_obj = sanssouci_obj_F,
+                                    pval_threshold = 1,
+                                    qfeature_obj = res1,
+                                    Foldchange_thlogFC = 1),
+               regexp = "Test function used must be  rowWelchTests as previously saved.")
+})
