841d6ceac6df47943d13678a1bb8947e0f4ae8be
nicolas-enjalbert <nicolasec@yahoo.fr>
2026-07-29 12:23:11 +0200

    Add as.SansSouci4DT function to create a SansSouci4DT class

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -16,3 +16,10 @@ test_that("Sanity checks of 'categCheck' throw errors when expected to", {
 
   expect_no_error(categCheck(c(1, 2, 3, 4), 4))
 })
+
+test_that("UT for as.SansSouci4DT", {
+  ss_obj <- sanssouci::SansSouciSim(m = 10, pi0 = 1, n = 5, rho = 1)
+  ss4DT_obj <- as.SansSouci4DT(ss_obj)
+  expect_equal(class(ss4DT_obj), c("SansSouci4DT", "SansSouci"))
+  expect_error(as.SansSouci4DT(1))
+})
